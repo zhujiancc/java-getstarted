65e2e3803862bd81f18c83276d04ecacca4809b8
朱坚 <850232649@qq.com>
2022-08-19 07:15:23 +0800

    init

---
+++ src/test/java/getstarted/ziana/com/GetstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package getstarted.ziana.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetstartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
